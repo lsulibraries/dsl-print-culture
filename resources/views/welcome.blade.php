031109e7ff9b95b58dcb0f272018c394e61c46ca
flummingbird <wwconlin@gmail.com>
2017-03-28 19:04:39 +0000

    put back control bar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,23 +7,21 @@
     <body>
         <div id="container" class="<?php echo $route; ?>">
             <div class="documentSection">
-                <div class="documentUnder"></div>           
-                <div class="documentOverflow"></div>        
+                <div class="documentUnder"></div>
+                <div class="documentOverflow"></div>
                 <div class="mainColumn">
-                    <!-- <control-bar></control-bar> -->
-                    <main-window ></main-window> 
+                    <control-bar></control-bar>
+                    <main-window ></main-window>
                     <!-- top-menu -->
                     <footer-bar></footer-bar>
-                </div>  
-                <issue-bar></issue-bar>          
+                </div>
+                <issue-bar></issue-bar>
             </div>
             <author-section class="authorSection"></author-section>
         </div>
-        
         <!-- <script src="//mozilla.github.io/pdf.js/build/pdf.js"></script> -->
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
         <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
-        
         <script src="js/app.js"></script>
         <script src="https://use.fontawesome.com/feda5854d8.js"></script>
 
