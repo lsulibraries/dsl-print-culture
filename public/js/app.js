9bbc0453a77ce3ed928c0069e27d2483cf20bb1c
Kyle Tanglao <ktanglao@lsu.edu>
2017-04-07 20:16:47 +0000

    Corrects nesting for author section

--- public/js/app.js
+++ public/js/app.js
@@ -427,6 +427,52 @@ Vue.component('author-section',{
 	 },
 	template: `
 		<div class="authorSection">
+
+                            <div class="authorLegend">
+                <div class="legendHeader">View authors and their mentions</div>
+                <div class="legendBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+
+                <div class="legendIcons">
+                    <div class="contributingIcon">
+                        <div class="legend1">
+                            <div class="innerIcon"></div>
+                        </div>
+                        <div class="legendLabels">
+                            <div class="iconTop">Contributing Author</div>
+                            <div class="iconBottom">High Frequency</div>
+                        </div>
+                    </div>
+                    <div class="contributingIcon">
+                        <div class="legend2">
+                            <div class="innerIcon"></div>
+                        </div>
+                        <div class="legendLabels">
+                            <div class="iconTop">Mentioned Author</div>
+                            <div class="iconBottom">High Frequency</div>
+                        </div>
+                    </div>
+                    <div class="contributingIcon">
+                        <div class="legend3">
+                            <div class="innerIcon"></div>
+                        </div>
+                        <div class="legendLabels">
+                            <div class="iconTop">Mentioned Author</div>
+                            <div class="iconBottom">Medium Frequency</div>
+                        </div>
+                    </div>    
+                    <div class="contributingIcon">
+                        <div class="legend4">
+                            <div class="innerIcon"></div>
+                        </div>
+                        <div class="legendLabels">
+                            <div class="iconTop">Mentioned Author</div>
+                            <div class="iconBottom">Low Frequency</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		
 			<div class="authorIntro"></div>
 			<div class="authorHeader">
                	<div class="inBorder"></div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,51 +26,9 @@
                 <issue-bar></issue-bar>
 
             </div>
-            <div class="authorLegend">
-                <div class="legendHeader">View authors and their mentions</div>
-                <div class="legendBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </div>
 
-                <div class="legendIcons">
-                    <div class="contributingIcon">
-                        <div class="legend1">
-                            <div class="innerIcon"></div>
-                        </div>
-                        <div class="legendLabels">
-                            <div class="iconTop">Contributing Author</div>
-                            <div class="iconBottom">High Frequency</div>
-                        </div>
-                    </div>
-                    <div class="contributingIcon">
-                        <div class="legend2">
-                            <div class="innerIcon"></div>
-                        </div>
-                        <div class="legendLabels">
-                            <div class="iconTop">Mentioned Author</div>
-                            <div class="iconBottom">High Frequency</div>
-                        </div>
-                    </div>
-                    <div class="contributingIcon">
-                        <div class="legend3">
-                            <div class="innerIcon"></div>
-                        </div>
-                        <div class="legendLabels">
-                            <div class="iconTop">Mentioned Author</div>
-                            <div class="iconBottom">Medium Frequency</div>
-                        </div>
-                    </div>    
-                    <div class="contributingIcon">
-                        <div class="legend4">
-                            <div class="innerIcon"></div>
-                        </div>
-                        <div class="legendLabels">
-                            <div class="iconTop">Mentioned Author</div>
-                            <div class="iconBottom">Low Frequency</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <author-section class="authorSection"></author-section>
+            <author-section class="authorSection">
+            </author-section>
         </div>
         <!-- <script src="//mozilla.github.io/pdf.js/build/pdf.js"></script> -->
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
