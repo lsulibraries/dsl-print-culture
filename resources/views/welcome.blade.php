0eb4f5599bf1689ae63205f8b5d0d8485350868f
flummingbird <wwconlin@gmail.com>
2017-05-03 18:33:29 +0000

    fix red-stuff on welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,28 +15,5 @@
         <script src="https://unpkg.com/vue@2.1.6/dist/vue.js"></script>
         <script src="js/app.js"></script>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
- 
-    <script>
-    $(function() {
-
-        var $sidebar   = $(".authorCard"), 
-            $window    = $(window),
-            offset     = $sidebar.offset(),
-            topPadding = 20;
-
-        $window.scroll(function() {
-            if ($window.scrollTop() > offset.top) {
-                $sidebar.stop().animate({
-                    marginTop: $window.scrollTop() - offset.top + topPadding
-                });
-            } else {
-                $sidebar.stop().animate({
-                    marginTop: 0
-                });
-            }
-        });
-        
-    });
-    </script>
     </body>
 </html>
