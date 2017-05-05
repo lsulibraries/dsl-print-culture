9c3e4ed10573091f0fc50c83ae41252bb1f53471
Jason Peak <jpeak5@lsu.edu>
2017-05-05 18:25:55 +0000

    tries and fails miserably to serve up a nice favico

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <meta charset="UTF-8">
     <head>
         <link rel="stylesheet" type="text/css" href="css/tachyon.css">
-        <link rel="icon" href="favicon_purple.ico">        
+        <link rel="icon" href="/favicon_purple.ico">        
         <title>The Broadway Journal</title>
         <script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>
         <script src="https://use.fontawesome.com/feda5854d8.js"></script>              
