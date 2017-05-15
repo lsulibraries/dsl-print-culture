d39b7a71ab42d77a716e9af1a71eeb1954cb301f
Jason Peak <jpeak5@lsu.edu>
2017-05-15 19:29:48 +0000

    updates path to search engine

--- storage/app/public/search.sh
+++ storage/app/public/search.sh
@@ -2,5 +2,5 @@
 OUTFILE=$1
 QUERY=$2
 
-java -cp /vagrant/saxon9he.jar net.sf.saxon.Query -o:$OUTFILE  -q:/var/www/dsl-print-culture/re\
+java -cp /opt/saxon9he.jar net.sf.saxon.Query -o:$OUTFILE  -q:/var/www/dsl-print-culture/re\
 sources/assets/xsl/string_search.xquery search_string=$QUERY
