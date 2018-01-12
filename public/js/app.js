46196ef951d6a7014597c8cb54c7975d9e8a384c
ktanglao <ktanglao@lsu.edu>
2018-01-12 21:51:05 +0000

    Personography adopts 30/70 layout, 4 authors across, still in progress

--- public/js/app.js
+++ public/js/app.js
@@ -323,9 +323,11 @@ Vue.component('biblPieceMeta', {
 Vue.component('personBibl', {
     template: `
 	<div class="personBibl">
-          <biblIssueMeta v-if="!this.$root.empty(bibl.issueMeta)" :issueMeta="bibl.issueMeta"></biblIssueMeta>
           <biblSectionMeta v-if="!this.$root.empty(bibl.sectionMeta)"  :sectionMeta="bibl.sectionMeta"></biblSectionMeta>
+          <div class="pieceTitleContainer">
           <biblPieceMeta v-if="!this.$root.empty(bibl.pieceMeta)"  :pieceMeta="bibl.pieceMeta" :issueId="bibl.issueMeta.issueId"></biblPieceMeta>
+          <biblIssueMeta v-if="!this.$root.empty(bibl.issueMeta)" :issueMeta="bibl.issueMeta"></biblIssueMeta>
+          </div>
           <biblPersonPieceMeta v-if="!this.$root.empty(bibl.personPieceMeta)" :personPieceMeta="bibl.personPieceMeta"></biblPersonPieceMeta>
         </div>
 	`,
