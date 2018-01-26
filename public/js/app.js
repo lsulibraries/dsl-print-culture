cd8933e5723e89b7cd3320b841ecd7d4008c0559
ktanglao <ktanglao@lsu.edu>
2018-01-26 20:16:55 +0000

    Adds role filter icons, removes text from appjs

--- public/js/app.js
+++ public/js/app.js
@@ -248,10 +248,10 @@ Vue.component('personFilter', {
             <div class="roleFilter">
                 <div class="labelFilter">Filter by role</div>
                 <div class="roles">
-                    <div class="roleFilterContributor"     v-bind:class="{active: roleFilter == 'cont'}" @click="updateRoleFilter('cont')">Contributor</div>
-                    <div class="roleFilterMentioned"     v-bind:class="{active: roleFilter == 'ment'}" @click="updateRoleFilter('ment')">Mentioned</div>
-                    <div class="roleFilterEditor"        v-bind:class="{active: roleFilter == 'edit'}" @click="updateRoleFilter('edit')">Editor</div>
-                    <div class="roleFilterCorrespondent" v-bind:class="{active: roleFilter == 'corr'}" @click="updateRoleFilter('corr')">Correspondent</div>
+                    <div class="roleFilterContributor"     v-bind:class="{active: roleFilter == 'cont'}" @click="updateRoleFilter('cont')"></div>
+                    <div class="roleFilterMentioned"     v-bind:class="{active: roleFilter == 'ment'}" @click="updateRoleFilter('ment')"></div>
+                    <div class="roleFilterEditor"        v-bind:class="{active: roleFilter == 'edit'}" @click="updateRoleFilter('edit')"></div>
+                    <div class="roleFilterCorrespondent" v-bind:class="{active: roleFilter == 'corr'}" @click="updateRoleFilter('corr')"></div>
                     <!-- <div class="numberFilterContributions" v-bind:class="{active: numFilter == 'num'}" @click="updateRoleFilter('num')">Contribution Number</div> -->
                 </div>
             </div>
