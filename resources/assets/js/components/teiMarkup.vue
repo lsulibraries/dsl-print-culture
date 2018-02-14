4ae6039f9b2649a457da590fed84b67308560adc
Jason Peak <jpeak5@lsu.edu>
2018-02-14 17:20:23 +0000

    factors masthead out of teiMarkup and into new component

--- resources/assets/js/components/teiMarkup.vue
+++ resources/assets/js/components/teiMarkup.vue
@@ -1,19 +1,6 @@
 <template>
         <div class='tei-markup'>
-            <div class="masthead" v-if="frontPage && masthead">
-                <div class="masthead-title">{{ this.mastheadIssueTitle }}</div>
-                <div class="masthead-issue">
-                    <div class="masthead-volume">{{ this.mastheadIssueVol }}</div>
-                    <div class="masthead-publication">{{ this.mastheadPublication }}</div>
-                    <div class="masthead-number">{{ this.mastheadIssueNum }}</div>
-                </div>
-                <div class="masthead-staff">
-                    <div :class="'masthead-' + role.toLowerCase()" v-for="(names,role) in this.mastheadPeopleByRole">
-                        <div class="masthead-role-label">{{ role }}</div>
-                        <div class="masthead-name" v-for="name in names">{{ name }}</div>
-                    </div>
-                </div>
-            </div>
+          <masthead></masthead>
             <vue-scrollbar class="issue-scrollbar" v-if="text">
               <div class='teiMarkup' v-html="text"></div>
             </vue-scrollbar>
@@ -21,9 +8,10 @@
 </template>
 <script>
     import VueScrollbar from './vue-scrollbar.vue'
+    import masthead from './masthead.vue'
 
     export default {
-        components: { VueScrollbar },
+        components: { VueScrollbar, masthead },
         props: ['issue', 'bibl'],
         created(){
             if (this.$route) {
@@ -37,29 +25,10 @@
                     this.biblId = false
                 }
             }
-            this.getMasthead()
             this.getText()
             this.parseRoles()
         },
         computed: {
-            frontPage: function () {
-                return this.$route.params.biblid ? false : true
-            },
-            mastheadIssueTitle: function() {
-                return this.masthead.issueTitle
-            },
-            mastheadIssueNum: function () {
-                return 'No. ' + this.masthead.issueNum
-            },
-            mastheadIssueVol: function () {
-                return 'Vol. ' + this.masthead.issueVol
-            },
-            mastheadPublication: function () {
-                return this.masthead.issueDate
-            },
-            mastheadPeople: function () {
-                return this.masthead.issueListPerson
-            },
             textLoaded: {
               get: function () {
                 let loaded = false;
@@ -111,17 +80,9 @@
               else {
                 this.setText()
               }
-              this.getMasthead()
-              this.$refs.Scrollbar.scrollToY(0)
-
-            },
-            getMasthead: function () {
-                let headerUrl = '/api/broadwayjournal/issue/'+ this.issueId +'/header';
-                axios.get(headerUrl).then(response => {
-                  this.masthead = response.data.issueMeta
-                  this.parseRoles()
-                });
-
+              if (this.$refs.Scrollbar) {
+                this.$refs.Scrollbar.scrollToY(0)
+              }
             },
             getText: function(){
                 if(this.biblId){
@@ -178,35 +139,6 @@
                     }
                 });
             },
-            parseRoles: function () {
-                if (!this.masthead.issueListPerson) {
-                    return []
-                }
-                let people = []
-                let groups = {}
-                for (const item of Object.values(this.mastheadPeople)) {
-                    if(Array.isArray(item)) {
-                        for(let i in item) {
-                            if(!groups.hasOwnProperty(item[i].personIssueRole)) {
-                                groups[item[i].personIssueRole] = []
-                            }
-                            groups[item[i].personIssueRole].push(item[i].personName)
-                        }
-                    }
-                    else {
-                        if(!groups.hasOwnProperty(item.personIssueRole)) {
-                            groups[item.personIssueRole] = []
-                        }
-                        groups[item.personIssueRole].push(item.personName)
-                    }
-                }
-                for (const [key, value] of Object.entries(groups)) {
-                    this.mastheadPeopleByRole[key] = []
-                    for (const name of Object.values(value)) {
-                        this.mastheadPeopleByRole[key].push(name)
-                    }
-                }
-            },
             setText: function () {
               if (this.textForRoute()) {
                 const leaf = this.$route.params.biblid ? this.$route.params.biblid : 'full'
@@ -228,10 +160,8 @@
             return{
                 issueId: '',
                 markdown:[],
-                masthead: {},
                 biblData: {},
                 biblId: false,
-                mastheadPeopleByRole: {},
                 text: ''
             }
         },
